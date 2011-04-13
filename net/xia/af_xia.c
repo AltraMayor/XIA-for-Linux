2f7c1241842f22dc0b14a9de8cb68bf7a9717042
Michel Machado <michel@digirati.com.br>
2011-04-13 13:35:21 -0400

    Added Makefiles for XIA

---
+++ net/xia/af_xia.c
@@ -0,0 +1,10 @@
+/*
+ * XIA 		An implementation of the XIA protocol suite for the LINUX
+ *		operating system.  XIA is implemented using the BSD Socket
+ *		interface as the means of communication with the user level.
+ *
+ *		PF_XIA protocol family socket handler.
+ *
+ * Author:	Michel Machado, <michel@digirati.com.br>
+ */
+
