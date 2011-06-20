312e21463a618926fd1b75d30d4545adc149bb1f
Michel Machado <michel@digirati.com.br>
2011-06-20 16:16:58 -0400

    Added essential code to receive user-land calls

---
+++ net/xia/util.c
@@ -0,0 +1,235 @@
+#include <asm/byteorder.h>
+#include <linux/kernel.h>
+#include <linux/ctype.h>
+#include <net/xia.h>
+
+#define EMPTY_EDGES 	(XIA_EMPTY_EDGE << 24 | XIA_EMPTY_EDGE << 16 |\
+			 XIA_EMPTY_EDGE <<  8 | XIA_EMPTY_EDGE)
+int xia_test_addr(const struct xia_addr *addr)
+{
+	int i, j, n;
+
+	/* Test that XIDTYPE_NAT is present only on last rows. */
+	{int saw_nat = 0;
+	n = XIA_NODES_MAX;
+	for (i = 0; i < XIA_NODES_MAX; i++) {
+		xid_type_t ty = addr->s_row[i].s_xid_type;
+		if (saw_nat) {
+			if (ty != XIDTYPE_NAT)
+				return XIAEADDR_NAT_MISPLACED;
+		} else if (ty == XIDTYPE_NAT) {
+			n = i;
+			saw_nat = 1;
+		}
+	}}
+	/* n = number of nodes from here. */
+
+	/* An empty DAG is not a valid address. */
+	if (n < 1)
+		return XIAEADDR_EMPTY;
+
+	BUILD_BUG_ON(XIA_OUTDEGREE_MAX != 4);
+
+	/* Test edges are well formed. */
+	for (i = 0; i < n; i++) {
+		const struct xia_row *row = &addr->s_row[i];
+		const u8 *edge = row->s_edge.a;
+		u32 all_edges = __be32_to_cpu(row->s_edge.i);
+		u32 bits = 0xffffffff;
+		for (j = 0; j < XIA_OUTDEGREE_MAX; j++, edge++) {
+			u8 e = *edge;
+			if (e & XIA_CHOSEN_EDGE) {
+				return XIAEADDR_CHOSEN_EDGE;
+			} else if (e == XIA_EMPTY_EDGE) {
+				if ((all_edges & bits) != (EMPTY_EDGES & bits))
+					return XIAEADDR_EE_MISPLACED;
+				else
+					break;
+			} else if (e >= n) {
+				return XIAEADDR_EDGE_OUT_RANGE;
+			}
+			bits >>= 8;
+		}
+	}
+
+	/* Test entry point is present. */
+	{__be32 all_edges = addr->s_row[n - 1].s_edge.i;
+	/* __be32_to_cpu is not necessary here! */
+	if (all_edges == EMPTY_EDGES) {
+		return XIAEADDR_NO_ENTRY;
+	}}
+
+	return XIAEADDR_OK;
+}
+EXPORT_SYMBOL(xia_test_addr);
+
+int xia_ntop(const struct xia_addr *src, char *dst, int dstlen)
+{
+	int i;
+	int tot = 0;
+	char *p = dst;
+	int left = dstlen;
+	char *ch_edge = "0123456789abcdefghijklmnopqrstuvwxyz";
+		/*       0123456789012345678901234567890123456789 */
+
+	BUILD_BUG_ON(sizeof(xid_type_t) != 4);
+	BUILD_BUG_ON(XIA_XID_MAX != 20);
+	BUILD_BUG_ON(XIA_OUTDEGREE_MAX != 4);
+	BUILD_BUG_ON(XIA_NODES_MAX >= 32);
+
+	for (i = 0; i < XIA_NODES_MAX && left > 0; i++) {
+		const struct xia_row *row = &src->s_row[i];
+		xid_type_t ty = row->s_xid_type;
+		const __be32 *pxid = (const __be32 *)row->s_xid;
+		u32 a = __be32_to_cpu(pxid[0]);
+		u32 b = __be32_to_cpu(pxid[1]);
+		u32 c = __be32_to_cpu(pxid[2]);
+		u32 d = __be32_to_cpu(pxid[3]);
+		u32 e = __be32_to_cpu(pxid[4]);
+		const u8 *edge = row->s_edge.a;
+		char e0 = ch_edge[edge[0] & 0x1f];
+		char e1 = ch_edge[edge[1] & 0x1f];
+		char e2 = ch_edge[edge[2] & 0x1f];
+		char e3 = ch_edge[edge[3] & 0x1f];
+		char *se0 = edge[0] & XIA_CHOSEN_EDGE ? ">" : "";
+		char *se1 = edge[1] & XIA_CHOSEN_EDGE ? ">" : "";
+		char *se2 = edge[2] & XIA_CHOSEN_EDGE ? ">" : "";
+		char *se3 = edge[3] & XIA_CHOSEN_EDGE ? ">" : "";
+		char *sep = i > 0 ? ":" : "";
+		int count;
+		if (ty == XIDTYPE_NAT)
+			break;
+		count = snprintf(p, left,
+			"%s%x-%.8x%.8x%.8x%.8x%.8x-%s%c%s%c%s%c%s%c",
+			sep, __be32_to_cpu(ty), a, b, c, d, e,
+			se0, e0, se1, e1, se2, e2, se3, e3);
+		left -= count;
+		p += count;
+		tot += count;
+	}
+
+	if (left <= 0)
+		return -ENOSPC;
+	*p = '\0';
+	return tot;
+}
+EXPORT_SYMBOL(xia_ntop);
+
+/*
+ * xia_pton and its auxiliares functions
+ */
+
+static inline void next(const char **pp, int *pleft)
+{
+	(*pp)++;
+	(*pleft)--;
+}
+
+static int read_sep(const char **pp, int *pleft, char sep)
+{
+	if (*pleft <= 0 || **pp != sep)
+		return -1;
+	next(pp, pleft);
+	return 0;
+}
+
+static inline int ascii_to_int(char ch)
+{
+	if (ch >= '0' && ch <= '9') {
+		return ch - '0';
+	} else if (ch >= 'A' && ch <= 'Z') {
+		return ch - 'A'; 
+	} else if (ch >= 'a' && ch <= 'z') {
+		return ch - 'a';
+	} else
+		return 64;
+}
+
+static int read_be32(const char **pp, int *pleft, __be32 *value)
+{
+	u32 result = 0;
+	int i = 0;
+
+	while (*pleft >= 1 && isxdigit(**pp) && i < 8) {
+		result = (result << 4) + ascii_to_int(**pp);
+		next(pp, pleft);
+		i++;
+	}
+	*value = __cpu_to_be32(result);
+	return 0;
+}
+
+static int read_type(const char **pp, int *pleft, xid_type_t *pty)
+{
+	BUILD_BUG_ON(sizeof(xid_type_t) != 4);
+
+	if (read_be32(pp, pleft, pty))
+		return -1;
+	return 0;
+}
+
+static int read_xid(const char **pp, int *pleft, u8 *xid)
+{
+	int i;
+	__be32 *pxid = (__be32 *)xid;
+	BUILD_BUG_ON(XIA_XID_MAX != 20);
+
+	for (i = 0; i < 5; i++) {
+		if (read_be32(pp, pleft, pxid++))
+			return -1;
+	}
+	return 0;
+}
+
+static int read_edges(const char **pp, int *pleft, u8 *edge, int ignore_ce)
+{
+	int i;
+	for (i = 0; i < XIA_OUTDEGREE_MAX; i++) {
+		int ce = 0;
+		int e = XIA_EMPTY_EDGE;
+		if (*pleft >= 1 && **pp == '>') {
+			ce = ignore_ce ? 0 : XIA_CHOSEN_EDGE;
+			next(pp, pleft);
+		}
+		if (*pleft >= 1 && isalnum(**pp)) {
+			e = ascii_to_int(**pp);
+			next(pp, pleft);
+		}
+		else
+			return -1;
+		edge[i] = ce | e;
+	}
+	return 0;
+}
+
+static int read_row(const char **pp, int *pleft, struct xia_row *row,
+	int ignore_ce)
+{
+	if (read_type(pp, pleft, &row->s_xid_type))
+		return -1;
+	if (read_sep(pp, pleft, '-'))
+		return -1;
+	if (read_xid(pp, pleft, row->s_xid))
+		return -1;
+	if (read_sep(pp, pleft, '-'))
+		return -1;
+	if (read_edges(pp, pleft, row->s_edge.a, ignore_ce))
+		return -1;
+	return 0;
+}
+
+int xia_pton(const char *src, int srclen, struct xia_addr *dst, int ignore_ce)
+{
+	const char *p = src;
+	int left = srclen;
+	int i = 0;
+
+	do {
+		if (read_row(&p, &left, &dst->s_row[i], ignore_ce))
+			return -1;
+		if (++i >= XIA_NODES_MAX)
+			return -1;
+	} while (!read_sep(&p, &left, ':'));
+	return 0;
+}
+EXPORT_SYMBOL(xia_pton);
