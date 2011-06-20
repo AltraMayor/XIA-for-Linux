df613dba32362c21b124acbbd3ca3293c4d7eef7
Michel Machado <michel@digirati.com.br>
2011-06-20 16:16:58 -0400

    Added essential code to receive user-land calls

--- include/net/xia.h
+++ include/net/xia.h
@@ -1,6 +1,7 @@
-#ifndef _XIA_SOCK_H
-#define _XIA_SCOK_H
+#ifndef _XIA_H
+#define _XIA_H
 
+#include <linux/socket.h>
 #include <net/sock.h>
 
 /** struct xia_sock - representation of XIA sockets
@@ -47,4 +48,107 @@ struct xia_raw_sock {
 
 extern struct proto xia_raw_prot;
 
-#endif	/* _XIA_SOCK_H */
+/*
+ * XIA address
+ */
+
+/* XID types. */
+enum {
+  XIDTYPE_NAT = 0,		/* Not A Type				*/
+
+  				/* 0x01--0x0f reserved for future use	*/
+
+  XIDTYPE_AD  = 0x10,		/* Autonomous Domain			*/
+  XIDTYPE_HID = 0x11,		/* Host					*/
+  XIDTYPE_CID = 0x12,		/* Content				*/
+  XIDTYPE_SID = 0x13,		/* Service				*/
+
+  XIDTYPE_USER = 0xffffff00,	/* User defined XID			*/
+  XIDTYPE_MAX
+};
+
+/* Row or a node in a DAG. */
+#define XIA_OUTDEGREE_MAX	4
+#define XIA_XID_MAX		20
+typedef __be32 xid_type_t;
+struct xia_row {
+	xid_type_t	s_xid_type;		/* XID type		*/
+	__u8		s_xid[XIA_XID_MAX];	/* eXpressive IDentifier*/
+	union {
+		__u8	a[XIA_OUTDEGREE_MAX];
+		__be32	i;
+	} s_edge;				/* Out edges		*/
+};
+#define XIA_CHOSEN_EDGE		0x80
+#define XIA_EMPTY_EDGE		0x7f
+
+/* XIA address. */
+#define XIA_NODES_MAX		9
+struct xia_addr {
+	struct xia_row s_row[XIA_NODES_MAX];
+}; 
+
+/* Structure describing an XIA socket address. */
+struct sockaddr_xia {
+  sa_family_t		sxia_family;	/* Address family		*/
+  __u8			__pad0[2];	/* Ensure 32-bit alignment	*/
+  struct xia_addr	sxia_addr;	/* XIA address			*/
+
+  /* Pad to size of `struct __kernel_sockaddr_storage'. */
+  unsigned char		__pad1[_K_SS_MAXSIZE - sizeof(sa_family_t) -
+			sizeof(unsigned char) - sizeof(struct xia_addr)];
+};
+
+enum xia_addr_error {
+	XIAEADDR_OK = 0,
+	/* There's a non-XIDTYPE_NAT node after an XIDTYPE_NAT node. */
+	XIAEADDR_NAT_MISPLACED,
+	/* There is no nodes, address is empty. */
+	XIAEADDR_EMPTY,
+	/* Edge-selected bit is only valid in packets. */
+	XIAEADDR_CHOSEN_EDGE,
+	/* There's a non-empty edge after an Empty Edge.
+	 * This error can also occur if an empty edge is selected. */
+	XIAEADDR_EE_MISPLACED,
+	/* An edge of a node is out of range. */
+	XIAEADDR_EDGE_OUT_RANGE,
+	/* Entry node is not present. */
+	XIAEADDR_NO_ENTRY,
+};
+
+extern int xia_test_addr(const struct xia_addr *addr);
+
+/** XIA_MAX_STRADDR_SIZE - The maximum size of an XIA address as a string
+ *			   in bytes. It's a recomended size to call xia_ntop.
+ * It includes space for the type and name of a nodes
+ * in hexadecimal, the out-edges, the two separators (i.e. '-') per node,
+ * the edge-chosen sign (i.e. '>') for each selected edge,
+ * the node separators (i.e. ':'), and a string terminator (i.e. '\0').
+ */
+#define XIA_MAX_STRADDR_SIZE (XIA_NODES_MAX * \
+	((sizeof(xid_type_t) + XIA_XID_MAX + XIA_OUTDEGREE_MAX) * 2 + 2) + \
+	XIA_NODES_MAX)
+
+/** xia_ntop - convert an XIA address to a string.
+ * src can be ill-formed, but xia_ntop won't report error and will return
+ * a string that `approximates' that ill-formed address.
+ * Return
+ * 	-ENOSPC - The converted address string is truncated. It may, or not,
+ *		include the trailing '\0'.
+ *	Total number of written bytes, NOT including the trailing '\0'.
+ */
+extern int xia_ntop(const struct xia_addr *src, char *dst, int dstlen);
+
+/** xia_pton - Convert a string that represents an XIA addressesng into
+ *	binary (network) form.
+ * It doesn't not require the string src to be terminated by '\0'.
+ * If ignore_ce is true, the chosen edges are not marked in dst.
+ * Return
+ * 	-1 if the string can't be converted; zero otherwise.
+ *	Notice that even if the function is successful, the address may
+ *	still be invalid according to xia_test_addr.
+ */
+extern int xia_pton(const char *src, int srclen, struct xia_addr *dst,
+		int ignore_ce);
+
+#endif	/* _XIA_H */
