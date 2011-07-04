e05e3bfa111b1a792fa7ee6dea13222ee3a56ed5
Michel Machado <michel@digirati.com.br>
2011-07-04 16:25:24 -0400

    Implemented the first version of net/xia/af_xia.c
    The functions in net/xia/af_xia.c heavy follow those in net/ipv4/af_inet.c
    It only implements the necessary code for raw XIA sockets, other transport
    protocols will require reviews of this file.

--- include/linux/xia.h
+++ include/linux/xia.h
@@ -8,4 +8,19 @@ enum {
   XIPPROTO_MAX
 };
 
+struct xiphdr {
+	__u8	version;	/* XIP version				*/
+	__u8	nxt_hdr;	/* Next header				*/
+	__be16	payload_len; 	/* Length of the payload in bytes	*/
+	__u8	hop;	/* Number of remaining hops allowed		*/
+	__u8	nd;	/* Number of rows of the destination address	*/
+	__u8	ns;	/* Number of rows of the source address		*/
+	__u8	ln;	/* Last Node visited				*/
+	
+	/*
+	 * Destination address starts here, and is followed by
+	 * the source address.
+	 */
+};
+
 #endif	/* _LINUX_XIA_H */
