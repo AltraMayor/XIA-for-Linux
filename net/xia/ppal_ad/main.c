fe58f43468fefab6fd970cad819b1fd2e56539f9
Michel Machado <michel@digirati.com.br>
2011-10-06 15:42:15 -0400

    Added skeleton of AD principal module

---
+++ net/xia/ppal_ad/main.c
@@ -0,0 +1,28 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+/*
+ * xia_init - this function is called when the module is loaded.
+ * Returns zero if successfully loaded, nonzero otherwise.
+ */
+static int __init xia_init(void)
+{
+	printk(KERN_ALERT "XIA Principal AD loaded\n");
+	return 0;
+}
+
+/*
+ * xia_exit - this function is called when the modlule is removed.
+ */
+static void __exit xia_exit(void)
+{
+	printk(KERN_ALERT "XIA Principal AD UNloaded\n");
+}
+
+module_init(xia_init);
+module_exit(xia_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Michel Machado <michel@digirati.com.br>");
+MODULE_DESCRIPTION("XIA Autonomous Domain Principal");
