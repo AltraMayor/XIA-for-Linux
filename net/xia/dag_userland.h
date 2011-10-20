c98022d90fa0f92c6f8adbb51be66ae32b0a1aa9
Michel Machado <michel@digirati.com.br>
2011-10-19 22:38:46 -0400

    Last review of dag.c (third round)
    Fixed some bugs, added defines to compile in userland, and added map for
    Not A Type (NAT) principal when XIA module initializes.

---
+++ net/xia/dag_userland.h
@@ -0,0 +1,91 @@
+#ifdef __KERNEL__
+
+#include <linux/ctype.h>
+#include <linux/spinlock.h>
+
+#define mymalloc(n)	kmalloc(n, GFP_KERNEL)
+#define myfree(p)	kfree(p)
+
+#else /* Userland */
+
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <asm-generic/errno-base.h>
+
+struct hlist_head {
+	struct hlist_node *first;
+};
+
+struct hlist_node {
+	struct hlist_node *next, **pprev;
+};
+
+#undef offsetof
+#ifdef __compiler_offsetof
+#define offsetof(TYPE,MEMBER) __compiler_offsetof(TYPE,MEMBER)
+#else
+#define offsetof(TYPE, MEMBER) ((size_t) &((TYPE *)0)->MEMBER)
+#endif
+
+#define container_of(ptr, type, member) ({			\
+	const typeof( ((type *)0)->member ) *__mptr = (ptr);	\
+	(type *)( (char *)__mptr - offsetof(type,member) );})
+
+#define hlist_entry(ptr, type, member) container_of(ptr,type,member)
+
+#define hlist_for_each_entry(tpos, pos, head, member)			 \
+	for (pos = (head)->first;					 \
+	     pos &&							 \
+		({ tpos = hlist_entry(pos, typeof(*tpos), member); 1;}); \
+	     pos = pos->next)
+#define hlist_for_each_entry_rcu	hlist_for_each_entry
+
+static inline void hlist_add_head(struct hlist_node *n, struct hlist_head *h)
+{
+	struct hlist_node *first = h->first;
+	n->next = first;
+	if (first)
+		first->pprev = &n->next;
+	h->first = n;
+	n->pprev = &h->first;
+}
+#define hlist_add_head_rcu	hlist_add_head
+
+static inline void __hlist_del(struct hlist_node *n)
+{
+	struct hlist_node *next = n->next;
+	struct hlist_node **pprev = n->pprev;
+	*pprev = next;
+	if (next)
+		next->pprev = pprev;
+}
+
+#define LIST_POISON1	NULL
+#define LIST_POISON2	NULL
+static inline void hlist_del(struct hlist_node *n)
+{
+	__hlist_del(n);
+	n->next = LIST_POISON1;
+	n->pprev = LIST_POISON2;
+}
+#define hlist_del_rcu		hlist_del
+
+#define mymalloc(n)	malloc(n)
+#define myfree(p)	free(p)
+
+#define BUG_ON(b)	assert(!(b))
+
+#define EXPORT_SYMBOL(x)
+
+#define spin_lock(x)
+#define spin_unlock(x)
+
+static inline void rcu_read_lock(void)		{ }
+static inline void rcu_read_unlock(void)	{ }
+static inline void synchronize_rcu(void)	{ }
+
+#define likely(b) (b)
+
+#endif /* __KERNEL__ */
