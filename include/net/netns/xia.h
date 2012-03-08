362d1324f44888b5e353c8c7660491d43167aca0
Michel Machado <michel@digirati.com.br>
2012-03-07 22:38:56 -0500

    First step to integrate XIP and DST subsystem
    - Added a DST table and struct dst_ops per net, but
      a single slab for struct xip_dst; this is similar to IPv6's approach.
    - Integrated the new DST tables with XIA's route infrastructure
      previously added.
    - Adopted net_ratelimit() for printk with warnings.
    
    NOTE: XIP doesn't route yet because it misses a lock mechanism
    for its DST tables, and its struct dst_ops' methods.

--- include/net/netns/xia.h
+++ include/net/netns/xia.h
@@ -5,12 +5,19 @@
 #ifndef __NETNS_XIA_H__
 #define __NETNS_XIA_H__
 
+#include <net/dst_ops.h>
+#include <net/xia_dst_table.h>
+
 struct netns_xia {
-	struct fib_xia_rtable *local_rtbl;
-	struct fib_xia_rtable *main_rtbl;
+	struct fib_xia_rtable	*local_rtbl;
+	struct fib_xia_rtable	*main_rtbl;
 #if defined(CONFIG_XIA_PPAL_HID) || defined(CONFIG_XIA_PPAL_HID_MODULE)
-	struct xia_hid_state *hid_state;
+	struct xia_hid_state	*hid_state;
 #endif
+
+	/* Route cache. */
+	struct dst_ops		xip_dst_ops;
+	struct xip_dst_table	xip_dst_table;
 };
 
 #endif /* __NETNS_XIA_H__ */
