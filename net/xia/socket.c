03cdea56a758bf8a7cb225f2123fc48b98842086
Michel Machado <michel@digirati.com.br>
2012-06-07 17:12:45 -0400

    Implement the first part of XDP principal
    - Move socket-related definitions from xia.h to xia_socket.h and
      ppal_raw/main.c.
    - Export route.c:xip_mark_addr_and_get_dst.
    - Allow principals to dynamically add socket protocols.
    - Review struct xia_sock.
    - Add the foundation of XDP principal. XDP is not running yet!

--- net/xia/socket.c
+++ net/xia/socket.c
@@ -1,36 +1,11 @@
 #include <linux/module.h>
+#include <net/xia_fib.h>
 #include <net/xia_dag.h>
-
-static void xia_sock_destruct(struct sock *sk)
-{
-	__skb_queue_purge(&sk->sk_receive_queue);
-	__skb_queue_purge(&sk->sk_error_queue);
-
-	sk_mem_reclaim(sk);
-
-	/* TODO How to include similar warning?
-	if (sk->sk_type == SOCK_STREAM && sk->sk_state != TCP_CLOSE) {
-		pr_err("Attempt to release TCP socket in state %d %p\n",
-		       sk->sk_state, sk);
-		dump_stack();
-		return;
-	} */
-	if (!sock_flag(sk, SOCK_DEAD)) {
-		pr_err("Attempt to release alive xia socket %p\n", sk);
-		dump_stack();
-		return;
-	}
-
-	WARN_ON(atomic_read(&sk->sk_rmem_alloc));
-	WARN_ON(atomic_read(&sk->sk_wmem_alloc));
-	WARN_ON(sk->sk_wmem_queued);
-	WARN_ON(sk->sk_forward_alloc);
-
-	dst_release(rcu_dereference_check(sk->sk_dst_cache, 1));
-	sk_refcnt_debug_dec(sk);
-}
+#include <net/xia_socket.h>
 
 /*
+ *	Support functions for principals
+ *
  * The routines beyond this point handle the behaviour of an AF_XIA
  * socket object. Mostly it punts to the subprotocols of XIP to do
  * the work.
@@ -41,13 +16,14 @@ static void xia_sock_destruct(struct sock *sk)
  * function we are destroying the object and from then on nobody
  * should refer to it.
  */
-static int xia_release(struct socket *sock)
+int xia_release(struct socket *sock)
 {
 	struct sock *sk = sock->sk;
 
 	if (sk) {
 		long timeout;
 
+		/* XXX Review this RPS call. */
 		sock_rps_reset_flow(sk);
 
 		/* If linger is set, we don't return until the close
@@ -66,70 +42,75 @@ static int xia_release(struct socket *sock)
 	}
 	return 0;
 }
-/* TODO review all exported symbols that are static! */
 EXPORT_SYMBOL_GPL(xia_release);
 
-/* XXX This code should be moved to an SID module. */
-#define XIDTYPE_SID (__cpu_to_be32(0x13))
-static int xia_bind(struct socket *sock, struct sockaddr *uaddr,
-		int addr_len)
+static int check_sockaddr_xia(struct sockaddr *uaddr, int addr_len)
 {
-	struct sock *sk = sock->sk;
-	struct sockaddr_xia *addr = (struct sockaddr_xia *)uaddr;
-	struct xia_sock *xia = xia_sk(sk);
-	int rc;
+	if (addr_len != sizeof(struct sockaddr_xia))
+		return -EINVAL;
+	if (uaddr->sa_family != AF_XIA)
+		return -EAFNOSUPPORT;
+	return 0;
+}
 
-	/* If the socket has its own bind function then use it. (RAW) */
-	if (sk->sk_prot->bind) {
-		rc = sk->sk_prot->bind(sk, uaddr, addr_len);
-		goto out;
+static int check_valid_single_sink(struct sockaddr_xia *addr)
+{
+	int i;
+	int n = xia_test_addr(&addr->sxia_addr);
+
+	if (n < 1) {
+		/* Invalid address since it's empty. */
+		return -EINVAL;
 	}
 
-	rc = -EINVAL;
-	if (addr_len < sizeof(struct sockaddr_xia))
-		goto out;
-	if (addr->sxia_family != AF_XIA)
-		goto out;
-	/* XXX We should support specific addresses, but for now
-	 * only autocomposed addresses are supported because
-	 * one has to still decide what is allowed.
-	 */
-	if (xia_test_addr(&addr->sxia_addr) != 1)
+	/* Verify that there's only one sink. */
+	i = n - 2;
+	while (i >= 0) {
+		__be32 all_edges = addr->sxia_addr.s_row[i].s_edge.i;
+		if (be32_to_raw_cpu(all_edges) == XIA_EMPTY_EDGES)
+			return -EINVAL; /* There's more than a sink. */
+	}
+		
+	return n;
+}
+
+int xia_bind(struct socket *sock, struct sockaddr *uaddr, int addr_len)
+{
+	DECLARE_SOCKADDR(struct sockaddr_xia *, addr, uaddr);
+	struct sock *sk;
+	struct xia_sock *xia;
+	int n, rc;
+
+	rc = check_sockaddr_xia(uaddr, addr_len);
+	if (rc)
 		goto out;
 
-	/* XXX The only XID type supported now is XIDTYPE_SID. */
-	rc = -EXTYNOSUPPORT;
-	if (addr->sxia_addr.s_row[0].s_xid.xid_type != XIDTYPE_SID)
+	rc = check_valid_single_sink(addr);
+	if (rc < 0)
 		goto out;
+	n = rc;
+
+	sk = sock->sk;
+	xia = xia_sk(sk);
 
 	lock_sock(sk);
 
-	/* Check these errors (active socket, double bind). */
-	rc = -EINVAL;
-	/* XXX How to add `sk->sk_state != TCP_CLOSE ||' for active socket? */
-	if (!xia_is_nat(xia->xia_sxid_type)) /* Double bind. */
-		goto out_release_sk;
-
-	xia->xia_sxid_type = addr->sxia_addr.s_row[0].s_xid.xid_type;
-	memmove(xia->xia_sxid, addr->sxia_addr.s_row[0].s_xid.xid_id,
-		sizeof(xia->xia_sxid));
-	/* Make sure we are allowed to bind here. */
-	rc = -EADDRINUSE;
-	/* The second parameter of method get_port is zero because XIA
-	 * doesn't use port numbers, and this method is shared with TCP/IP.
-	 */
-	if (sk->sk_prot->get_port(sk, 0)) {
-		xia->xia_sxid_type = __cpu_to_be32(XIDTYPE_NAT);
+	if (xia->xia_ssink) {
+		/* Double bind. */
+		rc = -EINVAL;
 		goto out_release_sk;
 	}
-
-	/* XXX A full address must be built not just copy the sink! */
 	xia->xia_saddr = addr->sxia_addr;
+	xia->xia_ssink = &xia->xia_saddr.s_row[n - 1];
 
-	sk->sk_userlocks |= SOCK_BINDADDR_LOCK | SOCK_BINDPORT_LOCK;
-	xia->xia_dxid_type = __cpu_to_be32(XIDTYPE_NAT);
-	sk_dst_reset(sk);
-	rc = 0;
+	rc = sk->sk_prot->bind(sk, uaddr, addr_len);
+	if (likely(!rc)) {
+		xia->xia_daddr_set = 0;
+		sk_dst_reset(sk);
+	} else {
+		xia->xia_ssink = NULL;
+	}
+
 out_release_sk:
 	release_sock(sk);
 out:
@@ -137,18 +118,22 @@ out:
 }
 EXPORT_SYMBOL_GPL(xia_bind);
 
-static int xia_dgram_connect(struct socket *sock, struct sockaddr *uaddr,
-		int addr_len, int flags)
+int xia_dgram_connect(struct socket *sock, struct sockaddr *uaddr,
+	int addr_len, int flags)
 {
-	struct sock *sk = sock->sk;
+	struct sock *sk;
+	struct xia_sock *xia;
+	int rc;
 
-	if (addr_len < sizeof(uaddr->sa_family))
+	rc = check_sockaddr_xia(uaddr, addr_len);
+	if (rc)
 		return -EINVAL;
-	if (uaddr->sa_family == AF_UNSPEC)
-		return sk->sk_prot->disconnect(sk, flags);
 
-	if (xia_is_nat(xia_sk(sk)->xia_sxid_type))
-		return -ESNOTBOUND;
+	sk = sock->sk;
+	xia = xia_sk(sk);
+
+	if (!xia->xia_ssink)
+		return -ESNOTBOUND; /* Please bind first! */
 
 	return sk->sk_prot->connect(sk, uaddr, addr_len);
 }
@@ -157,29 +142,29 @@ EXPORT_SYMBOL_GPL(xia_dgram_connect);
 /** copy_and_shade - Copy @src to @dst. The not used rows in @src are
  *  zeroed (shaded) in dst.
  *  This function is useful when passing an XIA address to userland because
- *  it ensures that there's no information leak due to unitilized memory.
+ *  it ensures that there's no information leak due to uninitialized memory.
  */
 static void copy_and_shade(struct xia_addr *dst, struct xia_addr *src)
 {
-	int i;
 	struct xia_row *rdst = dst->s_row;
 	struct xia_row *rsrc = src->s_row;
+	int i;
 
 	for (i = 0; i < XIA_NODES_MAX; i++) {
 		if (xia_is_nat(rsrc[i].s_xid.xid_type)) {
+			/* Shade it. */
 			memset(&rdst[i], 0,
 				(XIA_NODES_MAX - i) * sizeof(struct xia_row));
 			break;
 		}
 	}
+	/* Copy it. */
 	memmove(rdst, rsrc, i * sizeof(struct xia_row));
 }
 
-/*
- * This does both peername and sockname.
- */
-static int xia_getname(struct socket *sock, struct sockaddr *uaddr,
-		int *uaddr_len, int peer)
+/* This does both peername and sockname. */
+int xia_getname(struct socket *sock, struct sockaddr *uaddr,
+	int *uaddr_len, int peer)
 {
 	struct sock *sk		= sock->sk;
 	struct xia_sock *xia	= xia_sk(sk);
@@ -188,18 +173,14 @@ static int xia_getname(struct socket *sock, struct sockaddr *uaddr,
 	sxia->sxia_family = AF_XIA;
 	sxia->__pad0 = 0;
 	if (peer) {
-		/* XXX Is the following test copied from IPv4 necessary?
-		 || (((1 << sk->sk_state) & (TCPF_CLOSE | TCPF_SYN_SENT)) &&
-		     peer == 1)
-		*/
-		if (xia_is_nat(xia->xia_dxid_type))
+		if (!xia->xia_daddr_set)
 			return -ENOTCONN;
 		copy_and_shade(&sxia->sxia_addr, &xia->xia_daddr);
 	} else {
-		if (xia_is_nat(xia->xia_sxid_type))
-			memset(&sxia->sxia_addr, 0, sizeof(sxia->sxia_addr));
-		else
+		if (xia->xia_ssink)
 			copy_and_shade(&sxia->sxia_addr, &xia->xia_saddr);
+		else
+			memset(&sxia->sxia_addr, 0, sizeof(sxia->sxia_addr));
 	}
 
 	BUILD_BUG_ON(sizeof(sxia->__pad1));
@@ -213,17 +194,30 @@ static int xia_getname(struct socket *sock, struct sockaddr *uaddr,
 }
 EXPORT_SYMBOL_GPL(xia_getname);
 
-static int xia_ioctl(struct socket *sock, unsigned int cmd, unsigned long arg)
+int xia_ioctl(struct socket *sock, unsigned int cmd, unsigned long arg)
 {
 	struct sock *sk = sock->sk;
-	int rc = -ENOIOCTLCMD;
-	if (sk->sk_prot->ioctl)
-		rc = sk->sk_prot->ioctl(sk, cmd, arg);
+	int rc;
+
+	switch (cmd) {
+	case SIOCGSTAMP:
+		rc = sock_get_timestamp(sk, (struct timeval __user *)arg);
+		break;
+	case SIOCGSTAMPNS:
+		rc = sock_get_timestampns(sk, (struct timespec __user *)arg);
+		break;
+	default:
+		if (sk->sk_prot->ioctl)
+			rc = sk->sk_prot->ioctl(sk, cmd, arg);
+		else
+			rc = -ENOIOCTLCMD;
+		break;
+	}
 	return rc;
 }
 EXPORT_SYMBOL_GPL(xia_ioctl);
 
-static int xia_shutdown(struct socket *sock, int how)
+int xia_shutdown(struct socket *sock, int how)
 {
 	struct sock *sk = sock->sk;
 
@@ -253,27 +247,29 @@ static int xia_shutdown(struct socket *sock, int how)
 }
 EXPORT_SYMBOL_GPL(xia_shutdown);
 
-static int xia_sendmsg(struct kiocb *iocb, struct socket *sock,
-		struct msghdr *msg, size_t size)
+int xia_sendmsg(struct kiocb *iocb, struct socket *sock,
+	struct msghdr *msg, size_t size)
 {
 	struct sock *sk = sock->sk;
 
-	if (xia_is_nat(xia_sk(sk)->xia_sxid_type))
-		return -ESNOTBOUND;
-
+	/* XXX Review RPS calls. */
 	sock_rps_record_flow(sk);
+
+	if (!xia_sk(sk)->xia_ssink)
+		return -ESNOTBOUND;
 
 	return sk->sk_prot->sendmsg(iocb, sk, msg, size);
 }
 EXPORT_SYMBOL_GPL(xia_sendmsg);
 
-static int xia_recvmsg(struct kiocb *iocb, struct socket *sock,
-		struct msghdr *msg, size_t size, int flags)
+int xia_recvmsg(struct kiocb *iocb, struct socket *sock,
+	struct msghdr *msg, size_t size, int flags)
 {
 	struct sock *sk = sock->sk;
 	int addr_len = 0;
 	int rc;
 
+	/* XXX Review RPS calls. */
 	sock_rps_record_flow(sk);
 
 	rc = sk->sk_prot->recvmsg(iocb, sk, msg, size, flags & MSG_DONTWAIT,
@@ -284,15 +280,16 @@ static int xia_recvmsg(struct kiocb *iocb, struct socket *sock,
 }
 EXPORT_SYMBOL_GPL(xia_recvmsg);
 
-static ssize_t xia_sendpage(struct socket *sock, struct page *page,
-		int offset, size_t size, int flags)
+ssize_t xia_sendpage(struct socket *sock, struct page *page,
+	int offset, size_t size, int flags)
 {
 	struct sock *sk = sock->sk;
 
-	if (xia_is_nat(xia_sk(sk)->xia_sxid_type))
-		return -ESNOTBOUND;
-
+	/* XXX Review RPS calls. */
 	sock_rps_record_flow(sk);
+
+	if (!xia_sk(sk)->xia_ssink)
+		return -ESNOTBOUND;
 
 	if (sk->sk_prot->sendpage)
 		return sk->sk_prot->sendpage(sk, page, offset, size, flags);
@@ -300,74 +297,159 @@ static ssize_t xia_sendpage(struct socket *sock, struct page *page,
 }
 EXPORT_SYMBOL_GPL(xia_sendpage);
 
-/* XXX These constants must go away in order to support loadable principals. */
-/* XIP protocols. */
-enum {
-	XIPPROTO_XIP = 0,	/* eXpressive Internet Protocol		*/
-	XIPPROTO_RAW = 255,	/* Raw XIP packets			*/
-	XIPPROTO_MAX
-};
+/*
+ *	Principal-socket registering
+ */
+
+/* XXX This section is mostly copied from route.c for now, but
+ * I expect they will diverge as both, route.c and here, will evolve in
+ * different directions. If it doesn't happen, one should create
+ * a library to be used by both.	-- Michel Machado
+ */
+
+static DEFINE_SPINLOCK(ppal_lock);
+static struct hlist_head principals[NUM_PRINCIPAL_HINT];
+
+static inline struct hlist_head *ppalhead(xid_type_t ty)
+{
+	BUILD_BUG_ON_NOT_POWER_OF_2(NUM_PRINCIPAL_HINT);
+	return &principals[__be32_to_cpu(ty) & (NUM_PRINCIPAL_HINT - 1)];
+}
+
+static struct xia_socket_proc *find_sproc_locked(xid_type_t ty,
+	struct hlist_head *head)
+{
+	struct xia_socket_proc *sproc;
+	struct hlist_node *p;
+	hlist_for_each_entry(sproc, p, head, list)
+		if (sproc->ppal_type == ty)
+			return sproc;
+	return NULL;
+}
+
+static struct xia_socket_proc *find_sproc_rcu(xid_type_t ty,
+	struct hlist_head *head)
+{
+	struct xia_socket_proc *sproc;
+	struct hlist_node *p;
+	hlist_for_each_entry_rcu(sproc, p, head, list)
+		if (sproc->ppal_type == ty)
+			return sproc;
+	return NULL;
+}
+
+int xia_add_socket(struct xia_socket_proc *sproc)
+{
+	xid_type_t ty = sproc->ppal_type;
+	struct hlist_head *head = ppalhead(ty);
+	int rc;
+
+	spin_lock(&ppal_lock);
+
+	rc = -EEXIST;
+	if (find_sproc_locked(ty, head))
+		goto out;
+	hlist_add_head_rcu(&sproc->list, head);
+	rc = 0;
+
+out:
+	spin_unlock(&ppal_lock);
+	return rc;
+}
+EXPORT_SYMBOL_GPL(xia_add_socket);
+
+void xia_del_socket(struct xia_socket_proc *sproc)
+{
+	spin_lock(&ppal_lock);
+	hlist_del_rcu(&sproc->list);
+	spin_unlock(&ppal_lock);
+	synchronize_rcu();
+}
+EXPORT_SYMBOL_GPL(xia_del_socket);
+
+/*
+ *	Integration with socket API
+ */
+
+static void xia_sock_destruct(struct sock *sk)
+{
+	__skb_queue_purge(&sk->sk_receive_queue);
+	__skb_queue_purge(&sk->sk_error_queue);
+
+	sk_mem_reclaim(sk);
+
+	if (!sock_flag(sk, SOCK_DEAD)) {
+		pr_err("Attempt to release alive xia socket %p\n", sk);
+		dump_stack();
+		return;
+	}
+
+	WARN_ON(atomic_read(&sk->sk_rmem_alloc));
+	WARN_ON(atomic_read(&sk->sk_wmem_alloc));
+	WARN_ON(sk->sk_wmem_queued);
+	WARN_ON(sk->sk_forward_alloc);
+
+	dst_release(rcu_dereference_check(sk->sk_dst_cache, 1));
+	sk_refcnt_debug_dec(sk);
+}
 
 static int xia_create(struct net *net, struct socket *sock,
 		int protocol, int kern)
 {
+	xid_type_t ty = __cpu_to_be32(protocol);
+	struct hlist_head *head = ppalhead(ty);
+	struct xia_socket_proc *sproc;
+	const struct xia_socket_type_proc *stproc;
+	struct proto *chosen_prot;
 	struct sock *sk;
 	struct xia_sock *xia;
 	int rc;
 
 	sock->state = SS_UNCONNECTED;
 
+	/* Look for the requested type/protocol pair. */
 	rc = -ESOCKTNOSUPPORT;
-	if (sock->type != SOCK_RAW)
-		goto out;
-	rc = -EPROTONOSUPPORT;
-	if (protocol != XIPPROTO_XIP && protocol != XIPPROTO_RAW)
-		goto out;
-	protocol = XIPPROTO_RAW;
+	rcu_read_lock();
+	sproc = find_sproc_rcu(ty, head);
+	if (!sproc)
+		goto out_rcu_unlock;
+	stproc = sproc->procs[sock->type];
+	if (!stproc)
+		goto out_rcu_unlock;
+	chosen_prot = stproc->prot;
+	sock->ops = stproc->ops;
+	rcu_read_unlock();
 
-	rc = -EPERM;
-	if (sock->type == SOCK_RAW && !kern && !capable(CAP_NET_RAW))
-		goto out;
-
-	/*
-	 * There's no reason why XIA doesn't support Network Namespaces
-	 * but implementation.
-	 */
-	rc = -EAFNOSUPPORT;
-	if (!net_eq(net, &init_net))
-		goto out;
-
-	/* TODO WARN_ON(xia_raw_prot.slab == NULL); */
+	WARN_ON(chosen_prot->slab == NULL);
 
 	rc = -ENOBUFS;
-	sk = NULL; /* TODO See line below. */
-	/* TODO sk = sk_alloc(net, PF_XIA, GFP_KERNEL, &xia_raw_prot); */
+	sk = sk_alloc(net, PF_XIA, GFP_KERNEL, chosen_prot);
 	if (!sk)
 		goto out;
 
 	sock_init_data(sock, sk);
-	sk->sk_destruct	   = xia_sock_destruct;
-	sk->sk_protocol	   = protocol;
-	sk->sk_backlog_rcv = sk->sk_prot->backlog_rcv;
-	sk->sk_no_check = 1;		/* Checksum off by default */
+	sk->sk_destruct		= xia_sock_destruct;
+	sk->sk_protocol		= protocol;
+	sk->sk_backlog_rcv	= sk->sk_prot->backlog_rcv;
+	sk->sk_no_check		= 1;	/* Checksum off by default */
 	sk_refcnt_debug_inc(sk);
-	/* TODO sock->ops = &xia_sockraw_ops; */
 
 	xia = xia_sk(sk);
-	xia->xia_sxid_type = __cpu_to_be32(XIDTYPE_NAT);
-	xia->xia_dxid_type = __cpu_to_be32(XIDTYPE_NAT);
+	xia->xia_ssink = NULL;
+	xia->xia_daddr_set = 0;
 
 	rc = 0;
 	if (sk->sk_prot->init) {
 		rc = sk->sk_prot->init(sk);
 		if (rc)
-			goto out_release_sk;
+			sk_common_release(sk);
 	}
+	goto out;
+
+out_rcu_unlock:
+	rcu_read_unlock();
 out:
 	return rc;
-out_release_sk:
-	sk_common_release(sk);
-	goto out;
 }
 
 static const struct net_proto_family xia_family_ops = {
