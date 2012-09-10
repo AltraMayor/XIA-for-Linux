38ecc5ebc051ab9193ea664ae67f32391dfc9bb8
Michel Machado <michel@digirati.com.br>
2012-09-09 21:28:22 -0400

    Review net/xia/output.c
    This review removes the last TODOs;
    Linux XIA now has its first running version! :)

--- include/net/xia_output.h
+++ include/net/xia_output.h
@@ -14,18 +14,26 @@ int xip_send_skb(struct sk_buff *skb);
 /* @skb must already have XIP header, and not have link layer header. */
 struct sk_buff *xip_trim_packet_if_needed(struct sk_buff *skb, u32 mtu);
 
-struct sk_buff *xip_finish_skb(struct sock *sk);
-
 /* Throw away all pending outbound data on the socket. */
 void xip_flush_pending_frames(struct sock *sk);
 
 struct xip_dst;
 
+/* xip_start_skb() creates a sock buffer and associates it to @sk.
+ *
+ * Call xip_append_data() to add data to the sock buffer.
+ *
+ * Once all pieces of data are added, call xip_finish_skb()
+ * before consuming the datagram.
+ */
+int xip_start_skb(struct sock *sk, struct xip_dst *xdst,
+	const struct xia_addr *dest, int dest_n, u8 dest_last_node,
+	int transhdrlen, unsigned int flags);
 int xip_append_data(struct sock *sk,
 	int getfrag(void *from, char *to, int offset,
 		int len, int odd, struct sk_buff *skb),
-	struct iovec *from, int length, int transhdrlen,
-	struct xip_dst *xdst, unsigned int flags);
+	struct iovec *from, int length, unsigned int flags);
+struct sk_buff *xip_finish_skb(struct sock *sk);
 
 struct sk_buff *xip_make_skb(struct sock *sk,
 	const struct xia_addr *dest, int dest_n, u8 dest_last_node,
