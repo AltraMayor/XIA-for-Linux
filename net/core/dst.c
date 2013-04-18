9354d62cf6da9abaa5aafdeea9dbbc46b114938b
Michel Machado <michel@digirati.com.br>
2013-04-17 20:02:12 -0400

    net: run GC only when new dst entry is accounted
    Without this patch, GC of DST table run even for entries that are
    not supposed to be accounted.
    
    Given that this is a change in the core of Linux's network
    infrastruture, it should be discussed with maintainers to add, or
    not, it into mainline.
    
    XIP needs it because flag DST_NOCOUNT is used for dst entries
    that have a very short lifespan, and as such should not
    influence GC.
    
    Once dependency forest is fully implemented, this patch won't be
    necessary anymore, and can be removed.
    
    REMOVE ME WHEN POSSIBLE!
    
    Signed-off-by: Michel Machado <michel@digirati.com.br>

--- net/core/dst.c
+++ net/core/dst.c
@@ -202,7 +202,8 @@ void *dst_alloc(struct dst_ops *ops, struct net_device *dev,
 {
 	struct dst_entry *dst;
 
-	if (ops->gc && dst_entries_get_fast(ops) > ops->gc_thresh) {
+	if (ops->gc && !(flags & DST_NOCOUNT) &&
+		dst_entries_get_fast(ops) > ops->gc_thresh) {
 		if (ops->gc(ops))
 			return NULL;
 	}
