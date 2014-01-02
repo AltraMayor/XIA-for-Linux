e671ac78f43668c60e590db8792ccd7871ddbebb
Cody Doucette <doucette@bu.edu>
2014-01-02 13:48:43 -0500

    XIA/U4ID: add UDP socket for tunneling
    During the loading of the network namespace for the U4ID principal,
    a socket is created to serve as the entrance to a tunnel for any
    encapsulated XIP packets.
    
    By default, this socket is bound to INADDR_ANY:0x41d0 (0.0.0.0:16848),
    but can be configured by the network administrator using the kernel
    module parameters tunnel_addr (as an IPv4 address string) and
    tunnel_port (as a short):
    
     # modprobe xia_ppal_u4id tunnel_addr=10.0.2.15 tunnel_port=16500

--- net/xia/ppal_u4id/main.c
+++ net/xia/ppal_u4id/main.c
@@ -1,5 +1,7 @@
+#include <linux/inet.h>
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/moduleparam.h>
 #include <net/xia_dag.h>
 #include <net/xia_fib.h>
 #include <net/xia_vxidty.h>
@@ -12,6 +14,7 @@
 
 struct xip_u4id_ctx {
 	struct xip_ppal_ctx	ctx;
+	struct socket		*tunnel_sock;
 };
 
 static inline struct xip_u4id_ctx *ctx_u4id(struct xip_ppal_ctx *ctx)
@@ -27,6 +30,61 @@ static const xia_ppal_all_rt_eops_t u4id_all_rt_eops = {
 };
 
 /*
+ *	Tunnel socket
+ */
+
+/* INADDR_ANY must already be in network byte order. */
+#define DEF_TUNNEL_INET_ADDR	INADDR_ANY
+#define DEF_TUNNEL_UDP_PORT	0x41d0
+
+/* Parameters for configuring the IP address and
+ * port on which the tunnel socket is opened.
+ */
+static char	*tunnel_addr;
+static u16	tunnel_port = DEF_TUNNEL_UDP_PORT;
+
+module_param(tunnel_addr, charp, 0);
+MODULE_PARM_DESC(tunnel_addr, "IPv4 address for tunnel source");
+module_param(tunnel_port, short, 0);
+MODULE_PARM_DESC(tunnel_port, "UDP port for tunnel source");
+
+static inline void destroy_sock(struct socket *sock)
+{
+	kernel_sock_shutdown(sock, SHUT_RDWR);
+	sk_release_kernel(sock->sk);
+}
+
+static int create_u4id_tunnel_sock(struct xip_u4id_ctx *u4id_ctx,
+	struct net *net)
+{
+	struct socket *sock;
+	struct sockaddr_in sa;
+	int rc;
+
+	rc = __sock_create(net, AF_INET, SOCK_DGRAM, IPPROTO_UDP, &sock, 1);
+	if (rc)
+		goto out;
+
+	sa.sin_family = AF_INET;
+	sa.sin_addr.s_addr = tunnel_addr
+		? in_aton(tunnel_addr)
+		: DEF_TUNNEL_INET_ADDR;
+	sa.sin_port = htons(tunnel_port);
+
+	rc = kernel_bind(sock, (struct sockaddr *)&sa, sizeof(sa));
+	if (rc)
+		goto sock;
+
+	u4id_ctx->tunnel_sock = sock;
+	goto out;
+
+sock:
+	destroy_sock(sock);
+out:
+	return rc;
+}
+
+/*
  *	Network namespace
  */
 
@@ -36,12 +94,17 @@ static struct xip_u4id_ctx *create_u4id_ctx(void)
 	if (!u4id_ctx)
 		return NULL;
 	xip_init_ppal_ctx(&u4id_ctx->ctx, XIDTYPE_U4ID);
+	u4id_ctx->tunnel_sock = NULL;
 	return u4id_ctx;
 }
 
 /* IMPORTANT! Caller must RCU synch before calling this function. */
 static void free_u4id_ctx(struct xip_u4id_ctx *u4id_ctx)
 {
+	if (u4id_ctx->tunnel_sock) {
+		destroy_sock(u4id_ctx->tunnel_sock);
+		u4id_ctx->tunnel_sock = NULL;
+	}
 	xip_release_ppal_ctx(&u4id_ctx->ctx);
 	kfree(u4id_ctx);
 }
@@ -56,6 +119,13 @@ static int __net_init u4id_net_init(struct net *net)
 		rc = -ENOMEM;
 		goto out;
 	}
+
+	/* XXX A container should not be forced to have
+	 * a tunnel socket when it is not using XIA.
+	 */
+	rc = create_u4id_tunnel_sock(u4id_ctx, net);
+	if (rc)
+		goto u4id_ctx;
 
 	rc = init_xid_table(&u4id_ctx->ctx, net, &xia_main_lock_table,
 		u4id_all_rt_eops);
