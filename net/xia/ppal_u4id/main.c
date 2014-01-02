76afa7c810e37fd215bf579016dc07940b5b6126
Cody Doucette <doucette@bu.edu>
2014-01-02 15:25:23 -0500

    XIA/U4ID: add u4id_deliver and DST I/O functions
    If the U4ID to be routed is a local U4ID and it is not a sink, we
    have reached the destination of an XIP tunnel. Therefore, the last
    node should be advanced and routing may continue.
    
    If the U4ID to be routed has a valid XID but is not local, we
    need to tunnel.

--- net/xia/ppal_u4id/main.c
+++ net/xia/ppal_u4id/main.c
@@ -3,8 +3,10 @@
 #include <linux/module.h>
 #include <linux/moduleparam.h>
 #include <net/udp.h>
+#include <net/route.h>
 #include <net/xia_dag.h>
 #include <net/xia_fib.h>
+#include <net/xia_output.h>
 #include <net/xia_vxidty.h>
 #include <uapi/linux/udp.h>
 
@@ -17,6 +19,17 @@
 struct xip_u4id_ctx {
 	struct xip_ppal_ctx	ctx;
 	struct socket		*tunnel_sock;
+
+	/* Anchor for non-local, well-formed U4ID XIDs,
+	 * which represent tunnel destinations. Note
+	 * that an edge for this kind of XID will
+	 * always be followed, since the IP destination
+	 * will be assumed to be reachable.
+	 */
+	struct xip_dst_anchor	forward_anchor;
+
+	/* Anchor for ill-formed U4ID XIDs. */
+	struct xip_dst_anchor	ill_anchor;
 };
 
 static inline struct xip_u4id_ctx *ctx_u4id(struct xip_ppal_ctx *ctx)
@@ -268,16 +281,19 @@ static const xia_ppal_all_rt_eops_t u4id_all_rt_eops = {
 #define DEF_TUNNEL_INET_ADDR	INADDR_ANY
 #define DEF_TUNNEL_UDP_PORT	0x41d0
 
-/* Parameters for configuring the IP address and
- * port on which the tunnel socket is opened.
+/* Parameters for configuring the tunnel
+ * socket's IP address, port, and checksum.
  */
 static char	*tunnel_addr;
 static u16	tunnel_port = DEF_TUNNEL_UDP_PORT;
+static bool	disable_checksum;
 
 module_param(tunnel_addr, charp, 0);
 MODULE_PARM_DESC(tunnel_addr, "IPv4 address for tunnel source");
 module_param(tunnel_port, short, 0);
 MODULE_PARM_DESC(tunnel_port, "UDP port for tunnel source");
+module_param(disable_checksum, bool, 0);
+MODULE_PARM_DESC(disable_checksum, "Disable UDP checksum for tunnel source");
 
 static int create_u4id_tunnel_sock(struct xip_u4id_ctx *u4id_ctx,
 	struct net *net)
@@ -300,6 +316,9 @@ static int create_u4id_tunnel_sock(struct xip_u4id_ctx *u4id_ctx,
 	if (rc)
 		goto sock;
 
+	if (disable_checksum)
+		sock->sk->sk_no_check = UDP_CSUM_NOXMIT;
+
 	u4id_ctx->tunnel_sock = sock;
 	goto out;
 
@@ -319,6 +338,8 @@ static struct xip_u4id_ctx *create_u4id_ctx(void)
 	if (!u4id_ctx)
 		return NULL;
 	xip_init_ppal_ctx(&u4id_ctx->ctx, XIDTYPE_U4ID);
+	xdst_init_anchor(&u4id_ctx->forward_anchor);
+	xdst_init_anchor(&u4id_ctx->ill_anchor);
 	u4id_ctx->tunnel_sock = NULL;
 	return u4id_ctx;
 }
@@ -330,6 +351,8 @@ static void free_u4id_ctx(struct xip_u4id_ctx *u4id_ctx)
 		destroy_sock(u4id_ctx->tunnel_sock);
 		u4id_ctx->tunnel_sock = NULL;
 	}
+	xdst_free_anchor(&u4id_ctx->ill_anchor);
+	xdst_free_anchor(&u4id_ctx->forward_anchor);
 	xip_release_ppal_ctx(&u4id_ctx->ctx);
 	kfree(u4id_ctx);
 }
@@ -384,11 +407,247 @@ static struct pernet_operations u4id_net_ops __read_mostly = {
  *	U4ID Routing
  */
 
+
+/* Tunnel destination information held in a DST entry. */
+struct u4id_tunnel_dest {
+	__be32	dest_ip_addr;
+	__be16	dest_port;
+};
+
+static struct u4id_tunnel_dest *create_u4id_tunnel_dest(const u8 *xid)
+{
+	struct u4id_tunnel_dest *tunnel = kmalloc(sizeof(*tunnel), GFP_ATOMIC);
+	if (!tunnel)
+		return NULL;
+	tunnel->dest_ip_addr = *(__be32 *)xid;
+	tunnel->dest_port = *(__be16 *)(xid + sizeof(tunnel->dest_ip_addr));
+	return tunnel;
+}
+
+/* Callback function invoked when a DST entry is destroyed. */
+static void destroy_u4id_tunnel_dest(struct dst_entry *dst)
+{
+	kfree(dst_xdst(dst)->info);
+}
+
+/* Automatically called when the @skb is freed. */
+static void u4id_sock_free(struct sk_buff *skb)
+{
+	sock_put(skb->sk);
+}
+
+static int main_input_output(struct sk_buff *skb)
+{
+	struct net *net;
+	struct inet_sock *inet;
+	struct rtable *rt;
+	struct flowi4 fl4;
+	struct sock *sk;
+
+	struct xip_dst *xdst;
+	struct xip_u4id_ctx *u4id_ctx;
+	struct u4id_tunnel_dest *tunnel;
+
+	struct udphdr *uh;
+	int uhlen = sizeof(*uh);
+	int xip_and_data_len = skb->len;
+	__wsum csum = 0;
+	__be32 dest_ip_addr;
+	__be16 dest_port;
+
+	int rc;
+
+	/* Check that there's enough headroom in the @skb to
+	 * insert the IP and UDP headers. If not enough,
+	 * expand it to make room. Adjust truesize.
+	 */
+	if (skb_cow_head(skb, NET_SKB_PAD + sizeof(struct iphdr) + uhlen)) {
+		kfree_skb(skb);
+		return NET_XMIT_DROP;
+	}
+
+	skb_orphan(skb);
+
+	/* Reset @skb netfilter state. */
+	memset(&(IPCB(skb)->opt), 0, sizeof(IPCB(skb)->opt));
+	IPCB(skb)->flags &= ~(IPSKB_XFRM_TUNNEL_SIZE | IPSKB_XFRM_TRANSFORMED |
+		IPSKB_REROUTED);
+	nf_reset(skb);
+
+	/* Fetch U4ID from XDST entry to get IP address and port. */
+	xdst = skb_xdst(skb);
+	tunnel = (struct u4id_tunnel_dest *)xdst->info;
+	dest_ip_addr = tunnel->dest_ip_addr;
+	dest_port = tunnel->dest_port;
+
+	/* Get socket info. */
+	rcu_read_lock();
+	u4id_ctx = ctx_u4id(xip_find_ppal_ctx_rcu(skb_dst(skb)->dev->nd_net,
+		XIDTYPE_U4ID));
+	sk = u4id_ctx->tunnel_sock->sk;
+
+	inet = inet_sk(sk);
+
+	/* Set up UDP header. */
+	__skb_push(skb, uhlen);
+	skb_reset_transport_header(skb);
+	uh = udp_hdr(skb);
+	uh->source = inet->inet_sport;
+	uh->dest = dest_port;
+	uh->len = htons(uhlen + xip_and_data_len);
+
+	if (disable_checksum) {
+		skb->ip_summed = CHECKSUM_NONE;
+		uh->check = 0;
+	} else {
+		skb->ip_summed = CHECKSUM_COMPLETE;
+		csum = udp_csum(skb);
+		uh->check = csum_tcpudp_magic(inet->inet_saddr, dest_ip_addr,
+			skb->len - skb_transport_offset(skb),
+			sk->sk_protocol, csum);
+		if (uh->check == 0)
+			uh->check = CSUM_MANGLED_0;
+	}
+
+	/* Set up @skb. */
+	sock_hold(sk);
+	skb->sk = sk;
+	skb->protocol = __cpu_to_be16(ETH_P_IP);
+	skb->destructor = u4id_sock_free;
+	skb->local_df = 1;
+
+	/* Set up IP DST. */
+	skb_dst_drop(skb);
+	flowi4_init_output(&fl4, sk->sk_bound_dev_if, sk->sk_mark,
+		RT_TOS(inet->tos), RT_SCOPE_UNIVERSE, sk->sk_protocol,
+		inet_sk_flowi_flags(sk), dest_ip_addr, inet->inet_saddr,
+		dest_port, inet->inet_sport);
+	security_sk_classify_flow(sk, flowi4_to_flowi(&fl4));
+	net = sock_net(sk);
+	rt = ip_route_output_flow(net, &fl4, sk);
+	if (IS_ERR(rt)) {
+		rc = PTR_ERR(rt);
+		rt = NULL;
+		if (rc == -ENETUNREACH)
+			IP_INC_STATS_BH(net, IPSTATS_MIB_OUTNOROUTES);
+		goto out;
+	}
+	sk_dst_set(sk, dst_clone(&rt->dst));
+
+	/* Send UDP/IP packet with XIP and data as payload. */
+	rc = ip_queue_xmit(skb, flowi4_to_flowi(&fl4));
+out:
+	ip_rt_put(rt);
+	rcu_read_unlock();
+	return rc;
+}
+
+static int process_main_input(struct sk_buff *skb, int dec_hop_limit)
+{
+	/* XXX We should test that forwarding is enable per struct net.
+	 * See example in net/ipv6/ip6_output.c:ip6_forward.
+	 */
+	if (skb->pkt_type != PACKET_HOST)
+		goto drop;
+
+	if (dec_hop_limit) {
+		struct xiphdr *xiph = xip_hdr(skb);
+		if (!xiph->hop_limit) {
+			/* XXX Is this warning necessary? If so,
+			 * shouldn't it report more?
+			 */
+			LIMIT_NETDEBUG(
+				KERN_WARNING pr_fmt("%s: hop limit reached\n"),
+				__func__);
+			goto drop;
+		}
+		xiph->hop_limit--;
+	}
+
+	return 0;
+
+drop:
+	kfree_skb(skb);
+	return NET_RX_DROP;
+}
+
+static int main_input_input(struct sk_buff *skb)
+{
+	int rc = process_main_input(skb, 1);
+	if (rc)
+		return rc;
+	return dst_output(skb);
+}
+
+static int main_output_input(struct sk_buff *skb)
+{
+	int rc = process_main_input(skb, 0);
+	if (rc)
+		return rc;
+	return dst_output(skb);
+}
+
+#define main_output_output main_input_output
+
 static int u4id_deliver(struct xip_route_proc *rproc, struct net *net,
 	const u8 *xid, struct xia_xid *next_xid, int anchor_index,
 	struct xip_dst *xdst)
 {
-	return 0;
+	struct xip_ppal_ctx *ctx;
+	struct xip_u4id_ctx *u4id_ctx;
+	struct fib_xid *fxid;
+	struct u4id_tunnel_dest *tunnel;
+
+	rcu_read_lock();
+	ctx = xip_find_ppal_ctx_vxt_rcu(net, my_vxt);
+	u4id_ctx = ctx_u4id(ctx);
+
+	if (unlikely(!u4id_well_formed(xid))) {
+		/* This XID is malformed. */
+		xdst->passthrough_action = XDA_ERROR;
+		xdst->sink_action = XDA_ERROR;
+		xdst_attach_to_anchor(xdst, anchor_index,
+			&u4id_ctx->ill_anchor);
+		rcu_read_unlock();
+		return XRP_ACT_FORWARD;
+	}
+
+	fxid = xia_find_xid_rcu(ctx->xpc_xtbl, xid);
+	if (fxid) {
+		/* Reached tunnel destination; advance last node. */
+		struct fib_xid_u4id_local *lu4id = fxid_lu4id(fxid);
+		xdst->passthrough_action = XDA_DIG;
+		xdst->sink_action = XDA_ERROR; /* A U4ID cannot be a sink. */
+		xdst_attach_to_anchor(xdst, anchor_index, &lu4id->anchor);
+		rcu_read_unlock();
+		return XRP_ACT_FORWARD;
+	}
+
+	/* Assume an unknown, well-formed U4ID is a tunnel destination. */
+	tunnel = create_u4id_tunnel_dest(xid);
+	if (unlikely(!tunnel)) {
+		rcu_read_unlock();
+		/* Not enough memory to conclude this operation. */
+		return XRP_ACT_ABRUPT_FAILURE;
+	}
+	xdst->info = tunnel;
+	xdst->dst.ops->destroy = destroy_u4id_tunnel_dest;
+
+	xdst->passthrough_action = XDA_METHOD;
+	xdst->sink_action = XDA_ERROR;
+	BUG_ON(xdst->dst.dev);
+	xdst->dst.dev = net->loopback_dev;
+	dev_hold(xdst->dst.dev);
+	if (xdst->input) {
+		xdst->dst.input = main_input_input;
+		xdst->dst.output = main_input_output;
+	} else {
+		xdst->dst.input = main_output_input;
+		xdst->dst.output = main_output_output;
+	}
+	xdst_attach_to_anchor(xdst, anchor_index, &u4id_ctx->forward_anchor);
+	rcu_read_unlock();
+	return XRP_ACT_FORWARD;
 }
 
 static struct xip_route_proc u4id_rt_proc __read_mostly = {
