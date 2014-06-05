7cb48cf576b14ffe4f8ce0f037360f0a480c76ac
Michel Machado <michel@digirati.com.br>
2014-06-05 16:09:51 -0400

    XIA: add principal United 4ID
    United 4ID principal brings to Linux XIA compatibility with
    Click XIA's 4ID.
    
    For principal developers, United 4ID's code showcases
    routing redirects as a mean for principal composition since
    United 4ID in Linux XIA is a hollow principal that only
    delegates work to U4ID and I4ID principals.

---
+++ net/xia/ppal_uni4id/main.c
@@ -0,0 +1,199 @@
+#include <linux/module.h>
+#include <net/xia_fib.h>
+#include <net/xia_route.h>
+#include <net/xia_dag.h>
+#include <net/xia_vxidty.h>
+
+/* United 4ID Principal */
+#define XIDTYPE_UNI4ID (__cpu_to_be32(0x14))
+
+/*
+ *	United 4ID context
+ */
+
+struct xip_uni4id_ctx {
+	struct xip_ppal_ctx	ctx;
+
+	/* No extra field. */
+};
+
+static inline struct xip_uni4id_ctx *ctx_uni4id(struct xip_ppal_ctx *ctx)
+{
+	return likely(ctx)
+		? container_of(ctx, struct xip_uni4id_ctx, ctx)
+		: NULL;
+}
+
+static int my_vxt __read_mostly = -1;
+
+/*
+ *	Network namespace
+ */
+
+static struct xip_uni4id_ctx *create_uni4id_ctx(void)
+{
+	struct xip_uni4id_ctx *uni4id_ctx = kmalloc(
+		sizeof(*uni4id_ctx), GFP_KERNEL);
+	if (!uni4id_ctx)
+		return NULL;
+	xip_init_ppal_ctx(&uni4id_ctx->ctx, XIDTYPE_UNI4ID);
+	return uni4id_ctx;
+}
+
+/* IMPORTANT! Caller must RCU synch before calling this function. */
+static void free_uni4id_ctx(struct xip_uni4id_ctx *uni4id_ctx)
+{
+	xip_release_ppal_ctx(&uni4id_ctx->ctx);
+	kfree(uni4id_ctx);
+}
+
+static int __net_init uni4id_net_init(struct net *net)
+{
+	struct xip_uni4id_ctx *uni4id_ctx;
+	int rc;
+
+	uni4id_ctx = create_uni4id_ctx();
+	if (!uni4id_ctx) {
+		rc = -ENOMEM;
+		goto out;
+	}
+
+	rc = xip_add_ppal_ctx(net, &uni4id_ctx->ctx);
+	if (rc)
+		goto uni4id_ctx;
+	goto out;
+
+uni4id_ctx:
+	free_uni4id_ctx(uni4id_ctx);
+out:
+	return rc;
+}
+
+static void __net_exit uni4id_net_exit(struct net *net)
+{
+	struct xip_uni4id_ctx *uni4id_ctx =
+		ctx_uni4id(xip_del_ppal_ctx(net, XIDTYPE_UNI4ID));
+	free_uni4id_ctx(uni4id_ctx);
+}
+
+static struct pernet_operations uni4id_net_ops __read_mostly = {
+	.init = uni4id_net_init,
+	.exit = uni4id_net_exit,
+};
+
+/*
+ *	United 4ID Routing
+ */
+
+/* XXX The following XID types should come from their respective
+ * principals' header files once they are available.
+ */
+/* IP 4ID: XIP over IP. */
+#define XIDTYPE_I4ID (__cpu_to_be32(0x15))
+/* UDP 4ID: XIP over UDP. */
+#define XIDTYPE_U4ID (__cpu_to_be32(0x16))
+
+struct uni4id_xid {
+	u32	ip_addr;
+	u16	udp_port;
+	u16	zero1;
+	u32	zero2;
+	u32	zero3;
+	u32	zero4;
+};
+
+static int uni4id_deliver(struct xip_route_proc *rproc, struct net *net,
+	const u8 *xid, struct xia_xid *next_xid, int anchor_index,
+	struct xip_dst *xdst)
+{
+	struct uni4id_xid *from_xid;
+
+	BUILD_BUG_ON(sizeof(struct uni4id_xid) != XIA_XID_MAX);	
+
+	from_xid = (struct uni4id_xid *)xid;
+	if (from_xid->zero1 || from_xid->zero2 || from_xid->zero3 ||
+		from_xid->zero4) {
+		struct xip_ppal_ctx *ctx;
+
+		/* Get rid of misformed XIDs. */
+		xdst->passthrough_action = XDA_ERROR;
+		xdst->sink_action = XDA_ERROR;
+		rcu_read_lock();
+		ctx = xip_find_ppal_ctx_vxt_rcu(net, my_vxt);
+		xdst_attach_to_anchor(xdst, anchor_index, &ctx->negdep);
+		rcu_read_unlock();
+		return XRP_ACT_FORWARD;
+	}
+
+	/* Calculate next XID. */
+	next_xid->xid_type = from_xid->udp_port ? XIDTYPE_U4ID : XIDTYPE_I4ID;
+	memmove(next_xid->xid_id, xid, XIA_XID_MAX);
+	return XRP_ACT_REDIRECT;
+}
+
+static struct xip_route_proc uni4id_rt_proc __read_mostly = {
+	.xrp_ppal_type = XIDTYPE_UNI4ID,
+	.deliver = uni4id_deliver,
+};
+
+/*
+ * xia_uni4id_init - this function is called when the module is loaded.
+ * Returns zero if successfully loaded, nonzero otherwise.
+ */
+static int __init xia_uni4id_init(void)
+{
+	int rc;
+
+	rc = vxt_register_xidty(XIDTYPE_UNI4ID);
+	if (rc < 0) {
+		pr_err("Can't obtain a virtual XID type for United 4ID\n");
+		goto out;
+	}
+	my_vxt = rc;
+
+	rc = xia_register_pernet_subsys(&uni4id_net_ops);
+	if (rc)
+		goto vxt;
+
+	rc = xip_add_router(&uni4id_rt_proc);
+	if (rc)
+		goto net;
+
+	rc = ppal_add_map("uni4id", XIDTYPE_UNI4ID);
+	if (rc)
+		goto route;
+
+	pr_alert("XIA Principal United 4ID loaded\n");
+	goto out;
+
+route:
+	xip_del_router(&uni4id_rt_proc);
+net:
+	xia_unregister_pernet_subsys(&uni4id_net_ops);
+vxt:
+	BUG_ON(vxt_unregister_xidty(XIDTYPE_UNI4ID));
+out:
+	return rc;
+}
+
+/*
+ * xia_uni4id_exit - this function is called when the modlule is removed.
+ */
+static void __exit xia_uni4id_exit(void)
+{
+	ppal_del_map(XIDTYPE_UNI4ID);
+	xip_del_router(&uni4id_rt_proc);
+	xia_unregister_pernet_subsys(&uni4id_net_ops);
+	BUG_ON(vxt_unregister_xidty(XIDTYPE_UNI4ID));
+
+	rcu_barrier();
+
+	pr_alert("XIA Principal United 4ID UNloaded\n");
+}
+
+module_init(xia_uni4id_init);
+module_exit(xia_uni4id_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Michel Machado <michel@digirati.com.br>");
+MODULE_DESCRIPTION("XIA United 4ID Principal");
