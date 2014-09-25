bf7871cee386aab9d830c859895fc2c05e168aa6
Cody Doucette <doucette@bu.edu>
2014-09-25 15:16:17 -0400

    xia: style fixes for space around casts
    This patch complies with patch:
      - aad4f61498314d41d047ea2161b7bd7237b72d33
        checkpatch: add --strict tests for braces, comments and casts
    
    Ignored warnings that are false positives using the sizeof operator.

--- net/xia/dag_userland.h
+++ net/xia/dag_userland.h
@@ -27,11 +27,11 @@ struct hlist_node {
 #ifdef __compiler_offsetof
 #define offsetof(TYPE, MEMBER) __compiler_offsetof(TYPE, MEMBER)
 #else
-#define offsetof(TYPE, MEMBER) ((size_t) &((TYPE *)0)->MEMBER)
+#define offsetof(TYPE, MEMBER) ((size_t)&((TYPE *)0)->MEMBER)
 #endif
 
 #define container_of(ptr, type, member) ({			\
-	const typeof(((type *)0)->member) *__mptr = (ptr);	\
+	const typeof(((type *)0)->member)*__mptr = (ptr);	\
 	(type *)((char *)__mptr - offsetof(type, member)); })
 
 #define hlist_entry(ptr, type, member) container_of(ptr, type, member)
