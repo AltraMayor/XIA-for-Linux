57163e566743939a21f0ed8975553145321f5a14
saurav <sauravdahiya2010@gmail.com>
2017-06-04 22:52:47 +0530

    Adding ether.h header files

---
+++ include/net/xia_ether.h
@@ -0,0 +1,8 @@
+#ifndef _NET_XIA_ETHER_H
+#define _NET_XIA_ETHER_H
+/* prevents double declarations */
+
+#include <linux/netdevice.h>
+#include <net/xia_list_fib.h>
+
+#endif		/* _NET_XIA_ETHER_H */
