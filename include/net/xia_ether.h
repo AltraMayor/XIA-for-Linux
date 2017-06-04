09c66e54eb42ae484a4ba9f60be74aea7bd57d1a
saurav <sauravdahiya2010@gmail.com>
2017-06-04 22:55:23 +0530

    Adding local and main routing table fib-xids struct(incomplete)

--- include/net/xia_ether.h
+++ include/net/xia_ether.h
@@ -5,4 +5,25 @@
 #include <linux/netdevice.h>
 #include <net/xia_list_fib.h>
 
+/* Local ETHERs */
+
+struct fib_xid_ether_local {
+	struct xip_dst_anchor	xhl_anchor;
+
+	/* WARNING: @xhl_common is of variable size, and
+	 * MUST be the last member of the struct.
+	 */
+	struct fib_xid		xhl_common;
+};
+
+/* Main ETHERs */
+
+struct fib_xid_ether_main {
+
+	/* WARNING: @xhl_common is of variable size, and
+	 * MUST be the last member of the struct.
+	 */
+	struct fib_xid		xhl_common;
+};
+
 #endif		/* _NET_XIA_ETHER_H */
