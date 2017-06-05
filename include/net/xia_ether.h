4a72603f8b56d442d1e1ae39b7a23391d08003ec
saurav <sauravdahiya2010@gmail.com>
2017-06-05 08:59:10 +0530

    Adding ether context struct(incomplete)

--- include/net/xia_ether.h
+++ include/net/xia_ether.h
@@ -1,9 +1,15 @@
 #ifndef _NET_XIA_ETHER_H
 #define _NET_XIA_ETHER_H
 /* prevents double declarations */
+#ifdef __KERNEL__
+/* only for kernel use */
 
 #include <linux/netdevice.h>
 #include <net/xia_list_fib.h>
+
+/* Ethernet Principal */
+#define XIDTYPE_ETHER (__cpu_to_be32(0x12))
+
 
 /* Local ETHERs */
 
@@ -40,4 +46,11 @@ static inline struct fib_xid_ether_main *fxid_mether(struct fib_xid *fxid)
 		: NULL;
 }
 
+/* ETHER context */
+struct xip_ether_ctx
+{
+	struct xip_ppal_ctx ctx;
+};
+
+#endif /* __KERNEL__ */
 #endif		/* _NET_XIA_ETHER_H */
